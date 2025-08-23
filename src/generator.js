c00266d81800aa6871bff67c229cdd00525ab27f
M Ridho <mrdevs2026@gmail.com>
2025-08-23 07:27:45 +0700

    feat: add generator validation

--- src/generator.js
+++ src/generator.js
@@ -2,7 +2,26 @@ import { qrisgettagname, qrisparser } from "@misterdevs/qris-parser";
 import { toTLV } from "../helper/toTLV.js";
 import { generateCRC16 } from "../helper/generateCRC16.js";
 
-export function generator(qris, amount, feeType = 0, feeAmount) {
+export function generator(qris, amount, feeType, feeAmount) {
+  if (!qris) {
+    throw new Error("The qris must be have a value.");
+  }
+  if (amount && amount < 1) {
+    throw new Error("The amount value must bigger than 0.");
+  }
+
+  if (feeType && ![1, 2].includes(feeType)) {
+    throw new Error("The fee Type only support 1 or 2.");
+  }
+
+  if (feeType && !feeAmount) {
+    throw new Error("The fee amount value must bigger than 0.");
+  }
+
+  if (feeAmount && feeAmount < 1) {
+    throw new Error("The fee amount value must bigger than 0.");
+  }
+
   const qrisparse = qrisparser(qris);
 
   if (amount) {
@@ -19,7 +38,7 @@ export function generator(qris, amount, feeType = 0, feeAmount) {
   }
 
   //insert fee
-  if (feeType !== 0) {
+  if (feeType) {
     // feeType 2 for fixed and 3 for percentage
     const feeTag = feeType === 2 ? "56" : "57";
     const feeAmountPadded = String(feeAmount).padStart(2, "0");
